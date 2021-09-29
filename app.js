b3c8e5b4b2b758360d9b64f0dcd0abe46fb88284
milan.forrat <milan.forrat@gmail.com>
2021-09-29 11:58:40 +0200

    feat: added 2 new things to find + wip on score

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const ground = document.getElementById('ground');
 const wall = document.getElementById('wall');
 const light = document.getElementById('light');
-console.log(wall);
+const cable = document.getElementById('cable');
+const restaurantName = document.getElementById('restaurant-name');
+
+let score = 0;
 
 ground.addEventListener("click", function(){
     ground.style.transition ="0.5s";
@@ -17,3 +20,17 @@ light.addEventListener("click", function(){
     light.style.transition ="0.5s";
     light.style.opacity="0.75";
 });
+
+cable.addEventListener("click", function(){
+    cable.style.transition ="0.5s";
+    cable.style.opacity="1";
+});
+
+restaurantName.addEventListener("click", function(){
+    restaurantName.style.transition ="0.5s";
+    restaurantName.style.opacity="1";
+});
+
+
+
+console.log(score);
