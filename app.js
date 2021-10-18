b4a3d4fd356c24bae5f663b861096f91d5719fe9
milan.forrat <milan.forrat@gmail.com>
2021-10-18 11:58:50 +0200

    feat: start game btn + score logic

--- app.js
+++ app.js
@@ -3,34 +3,92 @@ const wall = document.getElementById('wall');
 const light = document.getElementById('light');
 const cable = document.getElementById('cable');
 const restaurantName = document.getElementById('restaurant-name');
+const gameStartBtn= document.getElementById('start-game');
+const imgOriginal = document.getElementById('game-italy-original');
+const imgError = document.getElementById('game-italy');
+const section = document.getElementsByTagName('section')[0];
 
+tabVar = [ground, wall, light, cable, restaurantName];
+tabScore = [];
 let score = 0;
 
-ground.addEventListener("click", function(){
-    ground.style.transition ="0.5s";
-    ground.style.opacity="1";
+gameStartBtn.addEventListener('click', function(){
+    play();
 });
 
-wall.addEventListener("click", function(){
-    wall.style.transition ="0.5s";
-    wall.style.opacity="1";
-});
+    // proposer un bouton rejouer quand gagné
 
-light.addEventListener("click", function(){
-    light.style.transition ="0.5s";
-    light.style.opacity="0.75";
-});
+function play(){
+    prepareGame();
+    listeners();
+}
 
-cable.addEventListener("click", function(){
-    cable.style.transition ="0.5s";
-    cable.style.opacity="1";
-});
+function prepareGame(){
+    section.transition = "1s";
+    section.style.visibility = "visible";
+    imgOriginal.style.transition ="2s";
+    imgError.style.transition ="2s";
+    imgOriginal.style.filter = "blur(0px)";
+    imgOriginal.style.filter = "saturate(100%)";
+    imgError.style.filter = "blur(0px)";
+    imgError.style.filter = "saturate(100%)";
+}
 
-restaurantName.addEventListener("click", function(){
-    restaurantName.style.transition ="0.5s";
-    restaurantName.style.opacity="1";
-});
+function listeners(){
+    // Après factorisation addEventListener
+    tabVar.forEach(element => {
+        element.addEventListener("click", function(){
+            element.style.transition ="0.5s";
+            element.style.opacity="1";
+            controlScore(element);
+        });
+    });
+}
 
+// fonction qui contrôle le tableau des scores
+function controlScore(element){
+    if(tabScore.includes(element)){
+        return;
+    }
+    else{
+        tabScore.push(element);
+        addScore();
+    }
+}
 
+// fonction qui incrémente le score
+function addScore(){
+    console.log(tabScore);
+    score = tabScore.length;
 
-console.log(score);
+    return score;
+}
+
+console.log("score : ",score);
+
+// Avant factorisation addEventListener
+
+// ground.addEventListener("click", function(){
+//     ground.style.transition ="0.5s";
+//     ground.style.opacity="1";
+// });
+
+// wall.addEventListener("click", function(){
+//     wall.style.transition ="0.5s";
+//     wall.style.opacity="1";
+// });
+
+// light.addEventListener("click", function(){
+//     light.style.transition ="0.5s";
+//     light.style.opacity="0.75";
+// });
+
+// cable.addEventListener("click", function(){
+//     cable.style.transition ="0.5s";
+//     cable.style.opacity="1";
+// });
+
+// restaurantName.addEventListener("click", function(){
+//     restaurantName.style.transition ="0.5s";
+//     restaurantName.style.opacity="1";
+// });
